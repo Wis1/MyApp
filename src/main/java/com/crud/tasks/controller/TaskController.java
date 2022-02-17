ed623960175309c63a46af834d49d86cb80263cd
Szymon Wizdorf <szymekwizdorf@gmail.com>
2022-02-17 18:56:22 +0100

    19.2 errors

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,37 +1,27 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+
 
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
-    }
-
-    @GetMapping(value = "{taskId}")
-    public TaskDto getTask(@PathVariable Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
-    }
-
-    @DeleteMapping
-    public void deleteTask(Long taskId) {
-
-    }
-
-    @PutMapping
-    public TaskDto updateTask(TaskDto taskDto) {
-        return new TaskDto(1L, "Edited test title", "Test content");
-    }
-
-    @PostMapping
-    public void createTask(TaskDto taskDto) {
-
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 }
