4babd259db35bada82cb5217f8587c18ac33734c
Szymon Wizdorf <szymekwizdorf@gmail.com>
2022-03-11 17:16:39 +0100

    22.3 add new task

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -23,12 +23,17 @@ public class TrelloController {
 
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.stream()
-                .filter(b->!b.getId().isEmpty()&&!b.getName().isEmpty())
-                        .filter(b->b.getName().contains("Kodilla"))
-                                .forEach(System.out::println);
-//        trelloBoards.forEach(trelloBoardDto -> {
-//            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-//        });
+//        trelloBoards.stream()
+//                .filter(b->!b.getId().isEmpty()&&!b.getName().isEmpty())
+//                        .filter(b->b.getName().contains("Kodilla"))
+//                                .forEach(System.out::println);
+
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
+            System.out.println("This board contains lists: ");
+            trelloBoardDto.getLists().forEach(trelloList -> {
+                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
+            });
+        });
     }
 }
