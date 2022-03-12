b514f64379994068cdf59981d13b0ce1e0fcab01
Szymon Wizdorf <szymekwizdorf@gmail.com>
2022-03-12 14:22:41 +0100

    22.4 added test shouldReturnEmptyList()

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -1,9 +1,13 @@
 package com.crud.tasks.domain;
 
-import lombok.Data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TrelloCardDto {
 
     private String name;
